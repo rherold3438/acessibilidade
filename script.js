fea3b35f675750de6fbf3cbcff073d9d22cacb4b
1trimeste <144137153+rherold3438@users.noreply.github.com>
2025-04-15 10:46:43 -0300

    rh

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', function(){
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
+    const alternaContraste = document.getElementsById('alternaContraste')
+
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
