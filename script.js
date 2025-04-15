90f05ce40286a8875ce585807a806aeda045c438
1trimeste <144137153+rherold3438@users.noreply.github.com>
2025-04-15 10:52:32 -0300

    rh

--- script.js
+++ script.js
@@ -16,5 +16,10 @@ document.addEventListener('DOMContentLoaded', function(){
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
 
     });
+    alternaContraste.addEventListener('click', function(){
 
-});
+    document.body.classList.toggle('alto-contraste')
+
+})
+
+})
